83e4383e5f49cc56b2193d6c0f26da1bc13d249d
izaberger <iza.berger2006@gmail.com>
2025-06-13 07:23:16 +0200

    Add files via upload

---
+++ rekomendacje.pl
@@ -0,0 +1,302 @@
+% ---------------------------
+% Fakty: wszystkie kierunki
+% ---------------------------
+
+kierunek('Informatyka',
+    [programowanie, technologie, matematyka, gry_komputerowe, elektronika],
+    [matematyka, informatyka, fizyka, angielski],
+    [logiczne_myslenie, kreatywnosc, dokladnosc, cierpliwosc],
+    [praktyka, projekty, samodzielna_nauka]).
+
+kierunek('Inżynieria danych',
+    [analiza_danych, matematyka, statystyka, technologie],
+    [matematyka, informatyka, statystyka, angielski],
+    [analityczne_myslenie, dokladnosc, systematycznosc],
+    [teoria, praktyka, samodzielna_nauka]).
+
+kierunek('Matematyka',
+    [matematyka, logika, rozwiazywanie_problemow, badania],
+    [matematyka, fizyka, informatyka],
+    [abstrakcyjne_myslenie, cierpliwosc, dokladnosc],
+    [teoria, samodzielna_nauka, rozwiazywanie_zadan]).
+
+kierunek('Fizyka',
+    [nauka, badania, kosmos, technologie],
+    [fizyka, matematyka, chemia, informatyka],
+    [ciekawosc, wytrwalosc, analityczne_myslenie],
+    [eksperymenty, teoria, praca_w_grupie]).
+
+kierunek('Automatyka i Robotyka',
+    [technologie, programowanie, elektronika, mechanika],
+    [matematyka, fizyka, informatyka],
+    [precyzja, kreatywnosc, logiczne_myslenie],
+    [praktyka, projekty, laboratoria]).
+
+kierunek('Medycyna',
+    [biologia, czlowiek, pomoc, nauka],
+    [biologia, chemia, fizyka, angielski],
+    [odpowiedzialnosc, empatia, odpornosc_na_stres],
+    [pamieciowa, praktyka, praca_w_grupie]).
+
+kierunek('Biotechnologia',
+    [biologia, chemia, badania, innowacje],
+    [biologia, chemia, matematyka, fizyka],
+    [ciekawosc, dokladnosc, cierpliwosc],
+    [laboratoria, projekty, praca_w_grupie]).
+
+kierunek('Weterynaria',
+    [zwierzeta, biologia, pomoc, przyroda],
+    [biologia, chemia, fizyka],
+    [empatia, cierpliwosc, odpornosc_na_stres],
+    [praktyka, pamieciowa, obserwacja]).
+
+kierunek('Ekonomia',
+    [finanse, biznes, analiza, matematyka],
+    [matematyka, geografia, wos, angielski],
+    [analityczne_myslenie, komunikatywnosc, przedsiebiorczosc],
+    [teoria, case_studies, dyskusje]).
+
+kierunek('Finanse i Rachunkowosc',
+    [pieniadze, analiza, biznes, matematyka],
+    [matematyka, wos, angielski],
+    [dokladnosc, systematycznosc, odpowiedzialnosc],
+    [praktyka, case_studies, samodzielna_nauka]).
+
+kierunek('Psychologia',
+    [czlowiek, zachowanie, pomoc, badania],
+    [biologia, wos, angielski],
+    [empatia, cierpliwosc, obserwacja],
+    [teoria, praktyka, dyskusje]).
+
+kierunek('Prawo',
+    [sprawiedliwosc, system, argumentacja, historia],
+    [historia, wos, polski, angielski],
+    [logiczne_myslenie, elokwencja, dokladnosc],
+    [pamieciowa, analiza, debaty]).
+
+kierunek('Inzynieria Ladowa',
+    [budownictwo, projektowanie, infrastruktura, matematyka],
+    [matematyka, fizyka, geografia],
+    [precyzja, przestrzenne_myslenie, odpowiedzialnosc],
+    [projekty, praktyka, obliczenia]).
+
+kierunek('Energetyka',
+    [technologie, ekologia, fizyka, innowacje],
+    [fizyka, matematyka, chemia],
+    [analityczne_myslenie, kreatywnosc, rozwiazywanie_problemow],
+    [laboratoria, projekty, symulacje]).
+
+kierunek('Inzynieria Materialowa',
+    [chemia, fizyka, technologie, badania],
+    [chemia, fizyka, matematyka],
+    [ciekawosc, dokladnosc, cierpliwosc],
+    [eksperymenty, laboratoria, analiza_danych]).
+
+kierunek('Lotnictwo i Kosmonautyka',
+    [technologie, mechanika, fizyka, przestrzen],
+    [fizyka, matematyka, informatyka],
+    [precyzja, logiczne_myslenie, odwage],
+    [symulacje, projekty, praktyka]).
+
+kierunek('Farmacja',
+    [chemia, biologia, medycyna, zdrowie],
+    [chemia, biologia, matematyka],
+    [dokladnosc, cierpliwosc, odpowiedzialnosc],
+    [laboratoria, pamieciowa, praktyka]).
+
+kierunek('Dietetyka',
+    [zdrowie, biologia, czlowiek, zywienie],
+    [biologia, chemia, wos],
+    [empatia, dokladnosc, komunikatywnosc],
+    [praktyka, case_studies, obserwacja]).
+
+kierunek('Ochrona Srodowiska',
+    [przyroda, ekologia, badania, zrownowazony_rozwoj],
+    [biologia, geografia, chemia],
+    [empatia, systematycznosc, krytyczne_myslenie],
+    [praca_w_terenie, projekty, badania]).
+
+kierunek('Biofizyka',
+    [biologia, fizyka, badania, nauka],
+    [fizyka, biologia, matematyka],
+    [analityczne_myslenie, ciekawosc, wytrwalosc],
+    [eksperymenty, teoria, modelowanie]).
+
+kierunek('Rachunkowosc',
+    [finanse, liczby, analiza, biznes],
+    [matematyka, wos, angielski],
+    [dokladnosc, systematycznosc, odpowiedzialnosc],
+    [obliczenia, case_studies, praktyka]).
+
+kierunek('Logistyka',
+    [organizacja, transport, planowanie, biznes],
+    [geografia, matematyka, wos],
+    [analityczne_myslenie, zaradnosc, komunikatywnosc],
+    [case_studies, symulacje, projekty]).
+
+kierunek('Bankowosc i Finanse',
+    [pieniadze, inwestycje, ekonomia, analiza],
+    [matematyka, wos, angielski],
+    [analityczne_myslenie, odpornosc_na_stres, przedsiebiorczosc],
+    [case_studies, symulacje, dyskusje]).
+
+kierunek('Filozofia',
+    [myslenie, etyka, logika, historia],
+    [historia, polski, wos],
+    [krytyczne_myslenie, ciekawosc, elokwencja],
+    [czytanie, dyskusje, eseje]).
+
+kierunek('Historia',
+    [przeszlosc, kultura, badania, spoleczenstwo],
+    [historia, polski, wos],
+    [pamiec, dokladnosc, krytyczne_myslenie],
+    [czytanie, analiza_zrodel, pisanie]).
+
+kierunek('Dziennikarstwo',
+    [media, komunikacja, pisanie, spoleczenstwo],
+    [polski, historia, wos],
+    [komunikatywnosc, ciekawosc, odwage],
+    [wywiady, pisanie, projekty]).
+
+kierunek('Stosunki Miedzynarodowe',
+    [polityka, gospodarka, dyplomacja, kultura],
+    [historia, wos, angielski],
+    [komunikatywnosc, elokwencja, analityczne_myslenie],
+    [dyskusje, case_studies, jezyki]).
+
+kierunek('Grafika',
+    [sztuka, design, technologie, kreatywnosc],
+    [plastyka, informatyka, historia_sztuki],
+    [kreatywnosc, precyzja, wyobraznia],
+    [praktyka, projekty, warsztaty]).
+
+kierunek('Architektura Wnetrz',
+    [design, przestrzen, sztuka, estetyka],
+    [plastyka, historia_sztuki, matematyka],
+    [kreatywnosc, precyzja, przestrzenne_myslenie],
+    [projekty, makietowanie, praktyka]).
+
+kierunek('Fotografia',
+    [sztuka, technologia, obserwacja, kreatywnosc],
+    [plastyka, informatyka, historia_sztuki],
+    [kreatywnosc, cierpliwosc, uwaga_na_szczegoly],
+    [praktyka, projekty, warsztaty]).
+
+kierunek('Filologia Germanska',
+    [jezyki, kultura, literatura, tlumaczenia],
+    [niemiecki, polski, historia],
+    [pamiec, komunikatywnosc, dokladnosc],
+    [czytanie, konwersacje, tlumaczenia]).
+
+kierunek('Filologia Romanska',
+    [jezyki, kultura, literatura, europa],
+    [francuski, polski, historia],
+    [komunikatywnosc, pamiec, elokwencja],
+    [czytanie, dyskusje, pisanie]).
+
+kierunek('Rolnictwo',
+    [przyroda, biologia, technologie, zywienie],
+    [biologia, chemia, geografia],
+    [praktycznosc, cierpliwosc, obserwacja],
+    [praktyka, eksperymenty, praca_w_terenie]).
+
+kierunek('Lesnictwo',
+    [przyroda, ekologia, ochrona_srodowiska, zarzadzanie],
+    [biologia, geografia, chemia],
+    [empatia, odpowiedzialnosc, wytrwalosc],
+    [praca_w_terenie, obserwacja, praktyka]).
+
+kierunek('Kryminologia',
+    [prawo, psychologia, spoleczenstwo, bezpieczenstwo],
+    [wos, historia, biologia],
+    [analityczne_myslenie, obserwacja, odpornosc_na_stres],
+    [case_studies, analiza, praktyka]).
+
+kierunek('Bezpieczenstwo Wewnetrzne',
+    [prawo, polityka, zarzadzanie, spoleczenstwo],
+    [wos, historia, geografia],
+    [odpowiedzialnosc, opanowanie, komunikatywnosc],
+    [case_studies, symulacje, praktyka]).
+
+kierunek('Wychowanie Fizyczne',
+    [sport, zdrowie, ruch, edukacja],
+    [biologia, wf, wos],
+    [aktywnosc, komunikatywnosc, cierpliwosc],
+    [praktyka, warsztaty, trening]).
+
+kierunek('Turystyka i Rekreacja',
+    [podroze, kultura, ruch, przyroda],
+    [geografia, wos, angielski],
+    [komunikatywnosc, zaradnosc, otwartosc],
+    [praktyka, projekty, wycieczki]).
+
+kierunek('Sztuczna Inteligencja',
+    [technologie, programowanie, matematyka, badania],
+    [matematyka, informatyka, fizyka],
+    [logiczne_myslenie, kreatywnosc, abstrakcyjne_myslenie],
+    [algorytmy, projekty, samodzielna_nauka]).
+
+kierunek('Cyberbezpieczenstwo',
+    [technologie, bezpieczenstwo, informatyka, analiza],
+    [informatyka, matematyka, wos],
+    [analityczne_myslenie, dokladnosc, rozwiazywanie_problemow],
+    [praktyka, case_studies, symulacje]).
+
+% ---------------------------
+% Wagi - jeśli chcesz zastosować
+% ---------------------------
+waga(zainteresowania, 3).
+waga(przedmioty, 2).
+waga(cechy, 2).
+waga(style, 1).
+
+% ---------------------------
+% Pomocnicze predykaty
+% ---------------------------
+
+wspolne_elementy([], _, 0).
+wspolne_elementy([X|Xs], Lista2, N) :-
+    member(X, Lista2),
+    !, wspolne_elementy(Xs, Lista2, N1),
+    N is N1 + 1.
+wspolne_elementy([_|Xs], Lista2, N) :-
+    wspolne_elementy(Xs, Lista2, N).
+
+dopasowanie(K, UZ, UP, UC, US, Wynik) :-
+    kierunek(K, Z, P, C, S),
+    wspolne_elementy(UZ, Z, W1),
+    wspolne_elementy(UP, P, W2),
+    wspolne_elementy(UC, C, W3),
+    wspolne_elementy(US, S, W4),
+    waga(zainteresowania, WZ), waga(przedmioty, WP),
+    waga(cechy, WC), waga(style, WS),
+    WynikValue is W1*WZ + W2*WP + W3*WC + W4*WS,
+    Wynik = [K, WynikValue].
+
+maks_dopasowanie(K, MaxW) :-
+    kierunek(K, Z, P, C, S),
+    waga(zainteresowania, WZ), waga(przedmioty, WP),
+    waga(cechy, WC), waga(style, WS),
+    length(Z, LZ), length(P, LP), length(C, LC), length(S, LS),
+    MaxW is LZ*WZ + LP*WP + LC*WC + LS*WS.
+
+compare_by_score(Delta, [_, P1], [_, P2]) :-
+    (P1 > P2 -> Delta = '<' ; Delta = '>').
+
+take(0, _, []) :- !.
+take(_, [], []) :- !.
+take(N, [X|Xs], [X|Ys]) :-
+    N1 is N - 1,
+    take(N1, Xs, Ys).
+
+top5_dopasowania(UZ, UP, UC, US, Top5) :-
+    findall([K, Procent],
+        (
+            dopasowanie(K, UZ, UP, UC, US, [K, W]),
+            maks_dopasowanie(K, M),
+            M > 0,
+            Procent is (W / M) * 100
+        ),
+        Wyniki),
+    predsort(compare_by_score, Wyniki, Posortowane),
+    take(5, Posortowane, Top5).
